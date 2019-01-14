86dda6c34ddadc7d47f13515b839501939dfa8e3
Rain <josiejosi@ymail.com>
2019-01-14 20:06:19 +0200

    User Model Block USer

--- app/Http/Controllers/BlockUserController.php
+++ app/Http/Controllers/BlockUserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User ;
+
 class BlockUserController extends Controller
 {
     public function __construct() { $this->middleware('auth') ; }
